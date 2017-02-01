670ae3eb0f004aae90920541ba019d14fd36811c
Hayato <joaopaulo.code@gmail.com>
2017-02-01 10:42:58 -0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
 class Config {
-	public $Crypt_Method		= 'AES-256-CBC';
-	public $Crypt_Key			= 'AES|NOMEDACHAVE'; //Altere o 'NOMEDACHAVE'
-	public $Crypt_IV			= '65SAF4156SAD165F'; //Altere mas mantenha 16 caracteres
+	public $Domain					= 'dominio.com'; //Altere para o seu domínio sem www.
+	public $Crypt_Method			= 'AES-256-CBC';
+	public $Crypt_Key				= 'AES|NOMEDACHAVE'; //Altere o 'NOMEDACHAVE'
+	public $Crypt_IV				= '65SAF4156SAD165F'; //Altere mas mantenha 16 caracteres
 }
 $Config = new Config();
-$MySQLi = new mysqli("localhost", "my_user", "my_password", "world");
+$MySQLi = new mysqli("localhost", "my_user", "my_password", "my_bank"); //Altere os dados para o seu banco MySQLi
