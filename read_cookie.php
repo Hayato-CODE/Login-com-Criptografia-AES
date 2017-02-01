fe9c61006cf6d20724db517ca8974443e1ee48c6
Hayato <joaopaulo.code@gmail.com>
2017-02-01 11:34:03 -0200

    Update read_cookie.php

--- read_cookie.php
+++ read_cookie.php
@@ -1,5 +1,7 @@
 <?php
+/* Includes Necessario */
 include('config.php');
 include('class.php');
 
-$Cookie_LOGIN = $Crypt->AES($_COOKIE['NOMEDOCOOKIE'], false); Altere o 'NOMEDOCOOKIE'
+/* Leitura do Cookie */
+$Cookie_LOGIN = $Crypt->AES($_COOKIE['NOMEDOCOOKIE'], false); //Altere o 'NOMEDOCOOKIE'
