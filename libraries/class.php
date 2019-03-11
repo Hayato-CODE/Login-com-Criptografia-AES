8fe90e7e3b2a53683d7807bf7c2a8fc7a6b9519a
João Paulo Borges e Silva <joao.paulo-bs@outlook.com>
2019-03-11 11:35:31 -0300

    Update class.php

--- libraries/class.php
+++ libraries/class.php
@@ -2,14 +2,14 @@
 /* Caso não saiba PHP Orientado a Objeto não altere este arquivo ou altere por conta e risco */
 
 /* Criptografia AES Própria */
-class Crypt {
-	public function AES($data, $function){
-		$Config = new Config();
-		if($function == true){
+class crypt {
+	public function aes($data, $encrypt){
+		$config = new config();
+		if ($encrypt == true) {
 			$data = bin2hex($data);
-			$return = openssl_encrypt($data, $Config->Crypt_Method, $Config->Crypt_Key, false, $Config->Crypt_IV);
-		}elseif($function == false){
-			$data = openssl_decrypt($data, $Config->Crypt_Method, $Config->Crypt_Key, false, $Config->Crypt_IV);
+			$return = openssl_encrypt($data, $config->crypt_method, $config->crypt_key, false, $config->crypt_iv);
+		} elseif ($encrypt == false) {
+			$data = openssl_decrypt($data, $config->crypt_method, $config->crypt_key, false, $config->crypt_iv);
 			$return = hex2bin($data);
 		}
 		return $return;
@@ -18,17 +18,17 @@ class Crypt {
 $Crypt = new Crypt();
 
 /* Proteção contra Injeção SQL */
-class SQL {
-	public function Injection($data){
+class sql {
+	public function injection($data){
 		$data = addslashes($data);
-		$data_Array = array(
+		$replace = array(
 			'SELECT' => '',
 			'UPDATE' => '',
 			'INSERT' => '',
 			'DROP' => ''
 		);
-		$return = str_replace(array_keys($data_Array), $data_Array, $data);
+		$return = str_replace(array_keys($replace), $replace, $data);
 		return $return;
 	}
 }
-$SQL = new SQL();
+$sql = new sql();
