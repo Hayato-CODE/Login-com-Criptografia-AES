a609c0a480d038574a9c03702078a52fa293c97b
João Paulo Borges e Silva <joao.paulo-bs@outlook.com>
2019-03-11 11:35:35 -0300

    Update config.php

--- libraries/config.php
+++ libraries/config.php
@@ -1,14 +1,16 @@
 <?php
-/* Configurações Necessarias */
-class Config {
-	public $Domain		= 'dominio.com'; //Altere para o seu domínio sem www.
-	public $Crypt_Method	= 'AES-256-CBC';
-	public $Crypt_Key	= 'AES|NOMEDACHAVE'; //Altere o 'NOMEDACHAVE'
-	public $Crypt_IV	= '65SAF4156SAD165F'; //Altere mas mantenha 16 caracteres
-	public $MySQLi_Host	= 'localhost'; //Altere para o servidor sql usado
-	public $MySQLi_User	= 'sql_user'; //Altere para o usuário sql usado
-	public $MySQLi_Pass	= 'sql_pass'; //Altere para o senha sql usada
-	public $MySQLi_Bank	= 'sql_bank'; //Altere para o banco sql usado
+/**
+ * Configurações Necessarias
+ */
+class config {
+	public $domain		= 'dominio.com'; //Altere para o seu domínio sem www.
+	public $crypt_method	= 'AES-256-CBC';
+	public $crypt_key	= 'AES|NOMEDACHAVE'; //Altere o 'NOMEDACHAVE'
+	public $crypt_iv	= '65SAF4156SAD165F'; //Altere mas mantenha 16 caracteres
+	public $mysqli_host	= 'localhost'; //Altere para o servidor sql usado
+	public $mysqli_user	= 'sql_user'; //Altere para o usuário sql usado
+	public $mysqli_pass	= 'sql_pass'; //Altere para o senha sql usada
+	public $mysqli_bank	= 'sql_bank'; //Altere para o banco sql usado
 }
-$Config = new Config();
-$MySQLi = new mysqli($Config->MySQLi_Host, $Config->MySQLi_User, $Config->MySQLi_Pass, $Config->MySQLi_Bank); //Altere os dados para o seu banco MySQLi
+$config = new config();
+$mysqli = new mysqli($config->mysqli_host, $config->mysqli_user, $config->mysqli_pass, $config->mysqli_bank);
