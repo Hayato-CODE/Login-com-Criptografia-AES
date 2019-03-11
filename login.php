8bd5278a27ac30fc3d1bfedfcefca2542623d388
João Paulo Borges e Silva <joao.paulo-bs@outlook.com>
2019-03-11 11:35:38 -0300

    Update login.php

--- login.php
+++ login.php
@@ -10,28 +10,28 @@ if (mysqli_connect_errno()) {
 }
 
 /* Checagem do $_POST do Formulário */
-if(isset($_POST)){
-	if($_POST['inputFORM'] === 'LoginSQL'){ // Insira o campo no seu formulário <input type="hidden" name="inputFORM" value="LoginSQL">
-		$Username = $SQL->Injection($_POST['username']);
-		$Password = $SQL->Injection($_POST['password']);
+if (isset($_POST)) {
+	if ($_POST['inputFORM'] === 'LoginSQL'){ // Insira o campo no seu formulário <input type="hidden" name="inputFORM" value="LoginSQL">
+		$username = $sql->injection($_POST['username']);
+		$password = $sql->injection($_POST['password']);
 		/* Checagem se a conta existe */
-		$Username_Query = 'SELECT * FROM login WHERE `username` LIKE `' . $Username . '` LIMIT 1';
-		$Username_Result = $mysqli->query($Username_Query);
-		$Username_Row = $Username_Result->fetch_array(MYSQLI_ASSOC);
-		if($Username_Row > 0){
+		$username_query = 'SELECT * FROM login WHERE `username` LIKE `' . $Username . '` LIMIT 1';
+		$username_result = $mysqli->query($username_query);
+		$username_row = $username_result->fetch_array(MYSQLI_ASSOC);
+		if ($username_row > 0) {
 			/* Checagem se a senha é a mesma do banco de dados */
-			$Password_Query = 'SELECT * FROM login WHERE `username` = "' . $Username . '" AND `password` = "' . $Password . '" LIMIT 1';
-			$Password_Result = $mysqli->query($Password_Query);
-			$Password_Row = $Password_Result->fetch_array(MYSQLI_ASSOC);
-			if($Password_Row > 0){
-				setcookie('Account', $Crypt->AES($Username, true), strtotime('+1 mouth'), '/', $Config->Domain, 1, 1);
+			$password_query = 'SELECT * FROM login WHERE `username` = "' . $username . '" AND `password` = "' . $password . '" LIMIT 1';
+			$password_result = $mysqli->query($password_query);
+			$password_row = $password_result->fetch_array(MYSQLI_ASSOC);
+			if ($password_row > 0) {
+				setcookie('account', $crypt->aes($username, true), strtotime('+1 mouth'), '/', $config->domain, 1, 1);
 				header('Location: /');
-			}else{
-				$Error_Password = true;
+			} else {
+				$error_password = true;
 			}
-		}else{
-			$Error_Username = true;
+		} else {
+			$error_username = true;
 		}
 	}
 }
-$MySQLi->close();
+$mysqli->close();
